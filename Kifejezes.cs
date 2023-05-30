735704cf7bc01dfaa9e3fad0e6296a380505c1b8
Progoktato <oktato.ber@gmail.com>
2023-05-30 18:13:56 +0200

    Add project files.

---
+++ Kifejezes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMuveletek
+{
+    public class Kifejezes
+    {
+        int operandusBal;
+        string muvelet;
+        int operandusJobb;
+
+        public Kifejezes(int operandusBal, string muvelet, int operandusJobb)
+        {
+            this.operandusBal = operandusBal;
+            this.muvelet = muvelet;
+            this.operandusJobb = operandusJobb;
+        }
+
+        public int OperandusBal { get => operandusBal; }
+        public string Muvelet { get => muvelet; }
+        public int OperandusJobb { get => operandusJobb; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleMuveletek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
